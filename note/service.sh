33af003fbdf520d0c0dcb276d32ab7cba6532d00
davidzzw <1315377399@qq.com>
2018-12-27 00:49:27 +0800

    去除cloud config配置 sevice.sh

--- note/service.sh
+++ note/service.sh
@@ -1,30 +1,55 @@
 #!/bin/sh  
 
 ## java env  
-export JAVA_HOME=/opt/app/jdk1.8.0_121
-export JRE_HOME=$JAVA_HOME/jre  
+#export JAVA_HOME="/usr/lib/java/jdk1.8.0_121"
+#export JRE_HOME="${JAVA_HOME}/jre  
+export JAVA_HOME="/usr/lib/java/jdk1.8.0_121/"
+export PATH="$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:"          
+export CLASS_PATH="$CLASSPATH:$JAVA_HOME/lib:$JAVA_HOME/jre/lib"
 
 ## service name  
-APP_NAME=trace-spark-handle
+SERVICE_NAME=trace-message
+# APP PATH
+SERVICE_PATH="/opt/trace-message/"
+JAR_NAME=${SERVICE_NAME}.jar
+PID=${SERVICE_NAME}.pid
 
-SERVICE_DIR=/root/trace-spark
-SERVICE_NAME=$APP_NAME
-JAR_NAME=$SERVICE_NAME\.jar
-PID=$SERVICE_NAME\.pid
-
-cd $SERVICE_DIR  
-
+#cd $SERVICE_PATH  
+if [ $# -eq 0 ]; then 
+    echo $"Usage: $0 {start|stop|restart}"
+    exit 0
+fi
 case "$1" in  
-
     start)  
-        nohup $JRE_HOME/bin/java -Xmn256m -Xms1024m -Xmx1024m -Duser.timezone=Asia/shanghai -Djava.rmi.server.useCodebaseOnly=true -XX:+DisableExplicitGC -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseFastAccessorMethods -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=5 -XX:+CMSParallelRemarkEnabled -XX:CMSInitiatingOccupancyFraction=60  -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSPermGenSweepingEnabled  -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=7 -XX:MaxTenuringThreshold=8 -XX:-UseBiasedLocking -XX:-UseAdaptiveSizePolicy -Djava.awt.headless=true -Duser.timezone=Asia/shanghai -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8003,suspend=n -jar $JAR_NAME >$SERVICE_DIR/$SERVICE_NAME\.log &  
-        echo $! > $SERVICE_DIR/$PID  
+	cd ${SERVICE_PATH}
+        nohup java -Xmn256m -Xms1024m -Xmx1024m  \
+			-Djava.rmi.server.useCodebaseOnly=true \
+			-XX:+DisableExplicitGC \
+			-XX:+UseParNewGC \
+			-XX:+UseConcMarkSweepGC \
+			-XX:+UseFastAccessorMethods \
+			-XX:+UseCMSCompactAtFullCollection \
+			-XX:CMSFullGCsBeforeCompaction=5 \
+			-XX:+CMSParallelRemarkEnabled \
+			-XX:CMSInitiatingOccupancyFraction=60  \
+			-XX:SoftRefLRUPolicyMSPerMB=0 \
+			-XX:+CMSPermGenSweepingEnabled  \
+			-XX:+CMSClassUnloadingEnabled \
+			-XX:SurvivorRatio=7 \
+			-XX:MaxTenuringThreshold=8 \
+			-XX:-UseBiasedLocking \
+			-XX:-UseAdaptiveSizePolicy \
+			-Djava.awt.headless=true \
+			-Duser.timezone=Asia/shanghai \
+			-jar $JAR_NAME >$SERVICE_PATH/$SERVICE_NAME\.log --spring.profiles.active=prod 2>&1 > /dev/null &  
+			#-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8005,suspend=n \
+        echo $! > $SERVICE_PATH/$PID  
         echo "=== start $SERVICE_NAME"  
         ;;  
       
     stop)  
-        kill `cat $SERVICE_DIR/$PID`  
-        rm -rf $SERVICE_DIR/$PID  
+        kill `cat $SERVICE_PATH/$PID`  
+        rm -rf $SERVICE_PATH/$PID  
         echo "=== stop $SERVICE_NAME"  
       
         sleep 5  
